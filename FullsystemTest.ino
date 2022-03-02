3f03f4079ad7bf12bef03899f9ac22463c58c05b
May-Aldhaiea <99053710+May-Aldhaiea@users.noreply.github.com>
2022-03-02 14:31:04 -0800

    Update FullsystemTest.ino

--- FullsystemTest.ino
+++ FullsystemTest.ino
@@ -81,5 +81,25 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+switch (stage){
+  case 1:
+  
+  break;
+  case 2:
+  
+  break;
+  case 3:
+  
+  break;
+  case 4:
+  
+  break;
+  case 1:
+  
+  break;
+  case 5:
+  
+  break;
+}
+delay(1000);
 }
