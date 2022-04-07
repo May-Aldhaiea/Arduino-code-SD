0fef2afb29a399cfe047f4bdc751ac1915f979a8
May-Aldhaiea <99053710+May-Aldhaiea@users.noreply.github.com>
2022-04-06 19:55:39 -0700

    Update suctionCupTest.ino

--- suctionCupTest.ino
+++ suctionCupTest.ino
@@ -1,64 +1,89 @@
 #include "Wire.h" //allows communication over i2c devices
 #include "Keyboard.h"
 
-const int pressureInput = A0; //select the analog input pin for the pressure transducer
-int solonoid1 = 12;
-int solonoid2 = 13;
+const int pressureInput = 11; //select the analog input pin for the pressure transducer
+const int solonoid1 = 12; //suction cup push
+const int solonoid2 = 13; // suction cup pull
+const int NPNsuc = 7; // npn sensor for the suction cup
+const int valve = 26; // valve for the vacuum  
 const int pressureZero = 102.4; //analog reading of pressure transducer at 0psi
 const int pressureMax = 921.6; //analog reading of pressure transducer at 100psi
 const int pressuretransducermaxPSI = 100; //psi value of transducer being used
-const int baudRate = 9600; //constant integer to set the baud rate for serial monitor
 const int sensorreadDelay = 250; //constant integer to set the sensor read delay in milliseconds
-bool CheckSolonoid = false;
-int inputTime = 0;
 float pressureValue = 0; //variable to store the value coming from the pressure transducer
-
+bool CheckSolonoid = false; // a state diagram to know if the suction cup are on or off
+bool vaccuum = false; // vaccuum state variable
+bool psi = false; // checks if the target psi is reached
+int stage = 0;
 void setup() //setup routine, runs once when system turned on or reset
 {
   Serial.begin(baudRate); //initializes serial communication at set baud rate bits per second
-  pinMode(solonoid1, OUTPUT);
-  pinMode(solonoid2, OUTPUT);
+  pinMode(solonoid1, OUTPUT); // suction cups
+  pinMode(solonoid2, OUTPUT);// put your setup code here, to run once:
+  pinMode(NPNsuc, INPUT);
+  pinMode(pressureInput, INPUT);
+  pinMode(valve, OUTPUT);
+
   digitalWrite(solonoid1, HIGH);
+  CheckSolonoid = digitalRead(NPNsuc);
+  while (CheckSolonoid == true)
+  {
+    CheckSolonoid = digitalRead(NPNsuc);
+    delay(5);
+  }
+  stage = 1;
 }
 
 void loop() //loop routine runs over and over again forever
 {
-  if(inputTime >= 20)
+  switch(stage)
   {
-    if (Serial.available() > 0) 
+    case 1:
+      // open vaccum valve
+    digitalWrite(valve,HIGH); 
+    while (psi == false) // get the vacuum to the proper PSI
     {
-  
-      // read incoming serial data:
-  
-      char inChar = Serial.read();
-      if (inChar == '\n')
-      {
-        if (CheckSolonoid == false)
-        {
-          digitalWrite(solonoid2, LOW);
-          delay(100);
-          digitalWrite(solonoid1, HIGH);
-          CheckSolonoid = true;
-        }else if (CheckSolonoid == true)
-        {
-          digitalWrite(solonoid1, LOW);
-          delay(100);
-          digitalWrite(solonoid2, HIGH);
-          CheckSolonoid = false;
-        }
-      }
-  
+      psi = digitalRead(pressureInput);
+      delay(5);
     }
-    inputTime = 0;
+    delay(1000);
+    //lower fixture
+    digitalWrite(solonoid1, LOW);
+    delay(100);
+    digitalWrite(solonoid2, HIGH);
+    CheckSolonoid = digitalRead(NPNsuc);
+    // wait for the solenoid to be down
+    while (CheckSolonoid = false)
+    {
+      CheckSolonoid = digitalRead(NPNsuc);
+      delay(5);
+    }
+    stage = 2;
+    break;
+    case 2:    
+    //raise  fixture
+    digitalWrite(solonoid2, LOW);
+    delay(100);
+    digitalWrite(solonoid1, HIGH);
+    CheckSolonoid = digitalRead(NPNsuc);
+    // wait for the solenoid to be up
+    while (CheckSolonoid = true)
+    {
+      CheckSolonoid = digitalRead(NPNsuc);
+      delay(5);
+    }
+    delay(1000);
+    // close vacuum
+    digitalWrite(valve,LOW); 
+    psi = digitalRead(pressureInput);
+    while (psi == true) // wait for the pressure to be gone
+    {
+      psi = digitalRead(pressureInput);
+      delay(5);
+    }
+    stage = 1;
+    break;
   }
-  pressureValue = analogRead(pressureInput); //reads value from input pin and assigns to variable
-  pressureValue = ((pressureValue-pressureZero)*pressuretransducermaxPSI)/(pressureMax-pressureZero); //conversion equation to convert analog reading to psi
-  Serial.print(pressureValue, 1); //prints value from previous line to serial
-  Serial.println("psi"); //prints label to serial
-  Serial.print("Pressure:"); //prints label
-  Serial.print(pressureValue, 1); //prints pressure value to lcd screen, 1 digit on float
-  Serial.print("psi"); //prints label after value
-  Serial.print("   "); //to clear the display after large values or negatives
-  inputTime = inputTime + 1;
-  delay(sensorreadDelay); //delay in milliseconds between read values
+
+  delay(1000); //delay in milliseconds between read values
 }
