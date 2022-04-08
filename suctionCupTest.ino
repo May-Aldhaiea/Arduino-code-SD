b8dde11c5b1fc6509abe263729481aa04eab3103
May-Aldhaiea <99053710+May-Aldhaiea@users.noreply.github.com>
2022-04-07 19:02:12 -0700

    Update suctionCupTest.ino

--- suctionCupTest.ino
+++ suctionCupTest.ino
@@ -1,6 +1,20 @@
 #include "Wire.h" //allows communication over i2c devices
 #include "Keyboard.h"
+// general
+const int PES = 28; // photoelectric sensor pin
+bool recieved = false; // checks if the substrate has been recieved 
 
+
+// clamps pins
+const int clampSLpush = 5; // clamp push
+const int clampSLpull = 6; // clamp pull
+const int NPN1 = 1; // left side sensor
+const int NPN2 = 2; // right side sensor
+bool Lclamps = true; // a state diagram to know if the left clamp is on or off
+bool Rclamps = true; // a state diagram to know if the right clamps is on or off
+
+// suction cup
+const int baudRate = 9600; //constant integer to set the baud rate for serial monitor
 const int pressureInput = 11; //select the analog input pin for the pressure transducer
 const int solonoid1 = 12; //suction cup push
 const int solonoid2 = 13; // suction cup pull
@@ -17,13 +31,22 @@ bool psi = false; // checks if the target psi is reached
 int stage = 0;
 void setup() //setup routine, runs once when system turned on or reset
 {
+  pinMode(PES,INPUT);
   Serial.begin(baudRate); //initializes serial communication at set baud rate bits per second
+  // suction cup
   pinMode(solonoid1, OUTPUT); // suction cups
   pinMode(solonoid2, OUTPUT);// put your setup code here, to run once:
   pinMode(NPNsuc, INPUT);
   pinMode(pressureInput, INPUT);
   pinMode(valve, OUTPUT);
+  
 
+  // valve
+  pinMode(clampSLpush, OUTPUT);
+  pinMode(clampSLpull, OUTPUT);
+  pinMode(NPN1,INPUT);
+  pinMode(NPN2,INPUT);
+  
   digitalWrite(solonoid1, HIGH);
   CheckSolonoid = digitalRead(NPNsuc);
   while (CheckSolonoid == true)
@@ -39,6 +62,12 @@ void loop() //loop routine runs over and over again forever
   switch(stage)
   {
     case 1:
+    recieved = digitalRead(PES);
+    while (recieved == false) // wait for the substrate to come from the EFEM
+    {
+      recieved = digitalRead(PES); // check if substrate been recieved
+      delay(1000);
+    }
       // open vaccum valve
     digitalWrite(valve,HIGH); 
     while (psi == false) // get the vacuum to the proper PSI
@@ -60,7 +89,60 @@ void loop() //loop routine runs over and over again forever
     }
     stage = 2;
     break;
-    case 2:    
+    case 2: 
+    // lower clamps
+    digitalWrite(clampSLpush, LOW);
+    delay(100);
+    digitalWrite(clampSLpull, HIGH);
+    Lclamps = digitalRead(NPN1);
+    Rclamps = digitalRead(NPN2);
+    // wait for the sensors to confirm that the clamps are down
+    while (Lclamps == false)
+    {
+      Lclamps = digitalRead(NPN1);
+      delay(5);
+    }
+    while (Rclamps == false)
+    {
+      Rclamps = digitalRead(NPN2);
+      delay(5);
+    }   
+    delay(1000);
+    // close vacuum
+    digitalWrite(valve,LOW); 
+    psi = digitalRead(pressureInput);
+    while (psi == true) // wait for the pressure to be gone
+    {
+      psi = digitalRead(pressureInput);
+      delay(5);
+    }
+    delay(5000);
+    // open vaccum valve
+    digitalWrite(valve,HIGH); 
+    while (psi == false) // get the vacuum to the proper PSI
+    {
+      psi = digitalRead(pressureInput);
+      delay(5);
+    }
+    delay(1000);
+    // raise clamps
+    digitalWrite(clampSLpull, LOW);
+    delay(100);
+    digitalWrite(clampSLpush, HIGH);
+    Lclamps = digitalRead(NPN1);
+    Rclamps = digitalRead(NPN2);
+    // wait for the sensors to confirm that the clamps are up
+    while (Lclamps == true)
+    {
+      Lclamps = digitalRead(NPN1);
+      delay(5);
+    }
+    while (Rclamps == true)
+    {
+      Rclamps = digitalRead(NPN2);
+      delay(5);
+    }
+    delay(500);
     //raise  fixture
     digitalWrite(solonoid2, LOW);
     delay(100);
@@ -81,6 +163,7 @@ void loop() //loop routine runs over and over again forever
       psi = digitalRead(pressureInput);
       delay(5);
     }
+    delay(5000);
     stage = 1;
     break;
   }
