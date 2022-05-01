1e6336ef894b994da6e71ed812649e4dfaa757a1
May-Aldhaiea <99053710+May-Aldhaiea@users.noreply.github.com>
2022-05-01 13:54:48 -0700

    Update doubleSolonoidTest.ino

--- doubleSolonoidTest.ino
+++ doubleSolonoidTest.ino
@@ -2,8 +2,10 @@
 #include "Keyboard.h"
 
 const int pressureInput = A0; //select the analog input pin for the pressure transducer
-int solonoid1 = 12;
-int solonoid2 = 13;
+int solonoid1 = 25;
+int solonoid2 = 27;
+int solonoid3 = 31;
+int solonoid4 = 29;
 const int pressureZero = 102.4; //analog reading of pressure transducer at 0psi
 const int pressureMax = 921.6; //analog reading of pressure transducer at 100psi
 const int pressuretransducermaxPSI = 100; //psi value of transducer being used
@@ -12,20 +14,24 @@ const int sensorreadDelay = 250; //constant integer to set the sensor read delay
 bool CheckSolonoid = false;
 int inputTime = 0;
 float pressureValue = 0; //variable to store the value coming from the pressure transducer
-
+int valve = 23;
 void setup() //setup routine, runs once when system turned on or reset
 {
   Serial.begin(baudRate); //initializes serial communication at set baud rate bits per second
   pinMode(solonoid1, OUTPUT);
   pinMode(solonoid2, OUTPUT);
+  pinMode(solonoid3, OUTPUT);
+  pinMode(solonoid4, OUTPUT);
   digitalWrite(solonoid1, HIGH);
+  pinMode(valve, OUTPUT);
+  //digitalWrite(solonoid3,LOW);
 }
 
 void loop() //loop routine runs over and over again forever
 {
   if(inputTime >= 8) //timer to switch which solonoid is active every 2 seconds
   {
-      
+      //digitalWrite(valve,HIGH);
        if (CheckSolonoid == false) // if this statement execute then push the solonoid
        {
          digitalWrite(solonoid2, LOW);
